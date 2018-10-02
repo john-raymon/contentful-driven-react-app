66c3ab41c89c67fa80997c16856f4a9d276b8752
John Ray <john@johnraymon.com>
2018-10-02 18:21:47 -0400

    refactored nav

---
+++ src/components/MobileMenu/index.js
@@ -0,0 +1,51 @@
+import React, { Component, Fragment} from 'react'
+import { Link } from 'react-router-dom'
+
+export default class MobileMenu extends Component {
+  render() {
+    const { position } = this.props;
+    return(
+      <div className="bg-black z-overlay fixed t0 l0 r0 b0 w100 h100 flex flex-column justify-center center"
+      style={{
+              WebkitTransform: `translate3d(${position}%, 0, 0)`,
+              transform: `translate3d(${position}%, 0, 0)`,
+            }}
+      >
+
+      <div class="absolute t0 r0 p3 pointer" onClick={() => this.props.toggle()}>
+        <p class="echomotors text-white bold text-md">
+          x
+        </p>
+      </div>
+      <div className="flex flex-column items-center max-width-4 mx-auto center">
+        <Link to="/announcements">
+          <p className="echomotors bold text-sm text-white center" onClick={() => this.props.toggle()}>
+            Announcements
+          </p>
+        </Link>
+        <Link to="/info">
+          <p className="echomotors bold text-sm text-white center" onClick={() => this.props.toggle()}>
+            Info
+          </p>
+        </Link>
+        <Link to="/visual">
+          <p className="echomotors bold text-sm text-white center" onClick={() => this.props.toggle()}>
+            Visual
+          </p>
+        </Link>
+        <Link to="/global">
+          <p className="echomotors bold text-sm text-white center" onClick={() => this.props.toggle()}>
+            Global
+          </p>
+        </Link>   
+        <Link to="/store">
+          <p className="echomotors bold text-sm text-white center" onClick={() => this.props.toggle()}>
+            Store
+          </p>
+        </Link>      
+      </div>
+
+      </div>
+    )
+  }
+}
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,64 +1,112 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom'; 
 import HamburgerIcon from './../../assets/svgs/HamburgerIcon'
+import Animate from 'react-move/Animate'
+import { easeExpOut } from 'd3-ease';
 import logo from 'assets/logo-white.png'
+import MobileMenu from 'components/MobileMenu'
 
-const Nav = () => (
-  <nav className="Nav fixed w100 pt2 pb2 px3 z-nav">
-    <div className="w100 flex flex-row items-center justify-between border-bottom border-medium border-white px1 mx-auto max-width-3">
-      <div className="text-decoration-none">
-        <Link to="/home">
-          <div className="Logo__container">
-            <img src={logo} className="w100" />  
+export default class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    }
+
+    this.toggleMenu = this.toggleMenu.bind(this)
+  }
+
+  toggleMenu() {
+    this.setState((state) => {
+      return { open: !state.open };
+    });
+  }
+
+  render() { 
+    return (
+      <nav className="Nav fixed w100 pt2 pb2 px3 z-nav">
+        <div className="w100 flex flex-row items-center justify-between border-bottom border-medium border-white px1 mx-auto max-width-3">
+          <div className="text-decoration-none">
+            <Link to="/home">
+              <div className="Logo__container">
+                <img src={logo} className="w100" />  
+              </div>
+            </Link>
           </div>
-        </Link>
-      </div>
+          <div className="HamburgerIcon__container fill-white" onClick={this.toggleMenu}> 
+            <HamburgerIcon />
+          </div>
 
-      <div className="max-width-4" style={{width: "66%"}}>
-        <ul class="list-reset flex flex-row overflow-scroll">
-          <li class="pl2 py2 flex-grow flex flex-column">
-            <a class="right" href="/announcements">
-              <p class="inline-block echomotors bold text-white right text-xxs">
-              Announcements
-              </p>
-            </a>     
-          </li>
-          <li class="pl2 py2 flex-grow flex flex-column"> 
-            <a class="right" href="/info">
-              <p class="inline-block echomotors bold text-white right text-xxs">
-                Info
-              </p>
-            </a>      
-          </li>
-          <li class="pl2 py2 flex-grow flex flex-column">
-            <a class="right" href="/visual">
-              <p class="inline-block echomotors bold text-white right text-xxs">
-                Visual
-              </p>
-            </a>      
-          </li>
-          <li class="pl2 py2 flex-grow flex flex-column"> 
-            <a class="right" href="/global">
-              <p class="inline-block echomotors bold text-white right text-xxs">
-                Global
-              </p>
-            </a>      
-          </li>
-          <li class="pl2 py2 flex-grow flex flex-column"> 
-            <a class="right" href="/store">
-              <p class="inline-block echomotors bold text-white right text-xxs">
-                Store
-              </p>
-            </a>      
-          </li>
-        </ul>
-      </div>
 
-    </div>
-  </nav>
-)
 
-export default Nav;
+
+          <Animate
+            start={() => ({
+              x: -100,
+            })}
+
+            update={() => ({
+              x: [this.state.open ? 0 : 100],
+              timing: { duration: 1000, ease: easeExpOut },
+            })}
+          >
+            {
+              (state) => {
+                const { x } = state;
+                return (
+                  <MobileMenu position={x} toggle={this.toggleMenu}/>
+                )
+              }
+             }
+          </Animate>
+
+
+
+          <div className="max-width-4 hide" style={{width: "66%"}}>
+            <ul class="list-reset flex flex-row overflow-scroll">
+              <li class="pl2 py2 flex-grow flex flex-column">
+                <a class="right" href="/announcements">
+                  <p class="inline-block echomotors bold text-white right text-xxs">
+                  Announcements
+                  </p>
+                </a>     
+              </li>
+              <li class="pl2 py2 flex-grow flex flex-column"> 
+                <a class="right" href="/info">
+                  <p class="inline-block echomotors bold text-white right text-xxs">
+                    Info
+                  </p>
+                </a>      
+              </li>
+              <li class="pl2 py2 flex-grow flex flex-column">
+                <a class="right" href="/visual">
+                  <p class="inline-block echomotors bold text-white right text-xxs">
+                    Visual
+                  </p>
+                </a>      
+              </li>
+              <li class="pl2 py2 flex-grow flex flex-column"> 
+                <a class="right" href="/global">
+                  <p class="inline-block echomotors bold text-white right text-xxs">
+                    Global
+                  </p>
+                </a>      
+              </li>
+              <li class="pl2 py2 flex-grow flex flex-column"> 
+                <a class="right" href="/store">
+                  <p class="inline-block echomotors bold text-white right text-xxs">
+                    Store
+                  </p>
+                </a>      
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </nav>
+    )}
+}
+
 
 // <div className="HamburgerIcon__container fill-white"> 
 //         <HamburgerIcon />
