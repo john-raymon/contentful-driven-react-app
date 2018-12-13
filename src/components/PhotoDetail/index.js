9bda3af31681246fa2738c8023418204541d71a8
John Ray <john@johnraymon.com>
2018-12-12 21:51:56 -0500

    on detail use contentful image api

--- src/components/PhotoDetail/index.js
+++ src/components/PhotoDetail/index.js
@@ -7,12 +7,12 @@ const PhotoDetail = ({visual}) => {
   const description = get(visual, 'description', '');
   const src = get(visual, 'photoFile.fields.file.url', '');
   const contentBlocks = get(visual, 'contentBlocks', []);
-  
+
   return (
     <div className='PhotoDetail mx-auto w100 max-width-3 px2'>
       <p className="echomotors text-sm bold text-white py2">{ title }</p>
       <div className="w100">
-        <img src={src} className="w100" />
+        <img src={`${src}?w=500`} className="w100" />
       </div>
       <p className="worksans text-sm text-white py2">
         { description }
